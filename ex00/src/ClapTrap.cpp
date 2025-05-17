af7ad1aaa8750c7af1db1e3bbc89485253eef349
Elroy <9432943+E-Andela@users.noreply.github.com>
2025-05-17 16:11:25 +0200

    add checks for hitpoints and energy points to member functions of ClapTrap

--- ex00/src/ClapTrap.cpp
+++ ex00/src/ClapTrap.cpp
@@ -3,7 +3,7 @@
 
 ClapTrap::ClapTrap(std::string name) : m_name {name}
 {
-	std::cout << "constructor called" << std::endl;
+	std::cout << "ClapTrap " << m_name << " constructed" << std::endl;
 }
 
 ClapTrap::ClapTrap(const ClapTrap& other)
@@ -12,14 +12,20 @@ ClapTrap::ClapTrap(const ClapTrap& other)
 	, m_energyPoints {other.m_energyPoints}
 	, m_attackDamage {other.m_attackDamage}
 {
-	std::cout << "copy constructor called" << std::endl;
+	std::cout << "New Claptrap constructed in the image of " << other.m_name << std::endl;
 }
 
 ClapTrap& ClapTrap::operator=(const ClapTrap& other)
 {
 	if (this == &other)
+	{
+		std::cout << "ClapTrap " << m_name << " is already in the image of " << other.m_name << std::endl;
+		std::cout << "No need to remodel" << std::endl;
 		return (*this);
+	}
 
+	std::cout << "ClapTrap " << m_name << " will be remodeled in the image of " << other.m_name << std::endl;
+	
 	m_name = other.m_name;
 	m_hitPoints = other.m_hitPoints;
 	m_energyPoints = other.m_energyPoints;
@@ -30,16 +36,51 @@ ClapTrap& ClapTrap::operator=(const ClapTrap& other)
 
 ClapTrap::~ClapTrap()
 {
-	std::cout << "destructing " << m_name << std::endl;
+	std::cout << "ClapTrap " << m_name << " disassembled" << std::endl;
 }
 
 void ClapTrap::attack(const std::string& target)
 {
-	std::cout << "ClapTrap " << m_name << " attacks " << target << " for " << m_attackDamage << " points of damage" << std::endl;
+	if (m_energyPoints > 0)
+	{
+		--m_energyPoints;
+		std::cout << "ClapTrap " << m_name << " attacks " << target << " for " << m_attackDamage << " points of damage" << std::endl;
+	}
+	else
+	{
+		std::cout << "ClapTrap " << m_name << " tries to attack, but has no energy left" << std::endl;
+	}	
 }
 
 void ClapTrap::takeDamage(unsigned int amount)
 {
+	if (m_hitPoints <= 0)
+	{
+		std::cout << "Stop! " << m_name << " is already dead" << std::endl;
+		return ;
+	}
 	m_hitPoints -= amount;
 	std::cout << "ClapTrap " << m_name << " takes " << amount << " points of damage" << std::endl;
+	if (m_hitPoints < 0)
+	{
+		m_hitPoints = 0;
+		std::cout << "ClapTrap " << m_name << " has lost all of its hit points and falls to the ground" << std::endl;
+	}
+	else
+		std::cout << "ClapTrap " << m_name << " has " << m_hitPoints << " hit points left" << std::endl;
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+	if (m_energyPoints > 0)
+	{
+		--m_energyPoints;
+		m_hitPoints += amount;
+		std::cout << "ClapTrap " << m_name << " regains " << amount << " hit points" << std::endl;
+		std::cout << "Claptrap " << m_name << " now has " << m_hitPoints << " hit points" << std::endl;
+	}
+	else
+	{
+		std::cout << "ClapTrap " << m_name << " tries to repair itself, but has no energy left" << std::endl;
+	}
 }
--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -1,4 +1,26 @@
+#include "../inc/ClapTrap.hpp"
+#include <iostream>
+
 int main()
 {
-	
+	ClapTrap ct1("R3-D4");
+	ClapTrap ct2("C3-PE");
+	ClapTrap ct3(ct1);
+	ClapTrap ct4("RT-DV");
+	ct4 = ct2;
+
+	std::cout << std::endl;
+
+	ct1.attack("C3-P0");
+	ct2.takeDamage(5);
+	ct2.attack("R3-D4");
+	ct2.beRepaired(3);
+
+	std::cout << std::endl;
+
+	ct4.takeDamage(5);
+	ct4.takeDamage(9);
+	ct4.takeDamage(5);
+
+	std::cout << std::endl;
 }
